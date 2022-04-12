28bcf0dc5f2bd34ba1f7994718d9ba16aaf7a763
Gabi Album <63543882+galbum@users.noreply.github.com>
2022-04-12 17:08:39 +0300

    Add files via upload

--- challenge/agoda_cancellation_estimator.py
+++ challenge/agoda_cancellation_estimator.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 from typing import NoReturn
 from IMLearn.base import BaseEstimator
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 
 
 class AgodaCancellationEstimator(BaseEstimator):
@@ -9,69 +11,60 @@ class AgodaCancellationEstimator(BaseEstimator):
     An estimator for solving the Agoda Cancellation challenge
     """
 
-    def __init__(self) -> AgodaCancellationEstimator:
+    def __init__(self):
         """
         Instantiate an estimator for solving the Agoda Cancellation challenge
-
         Parameters
         ----------
-
-
         Attributes
         ----------
-
         """
         super().__init__()
 
-    def _fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
+    def _fit(self, X: np.ndarray, y: np.ndarray,) -> NoReturn:
         """
         Fit an estimator for given samples
-
         Parameters
         ----------
         X : ndarray of shape (n_samples, n_features)
             Input data to fit an estimator for
-
         y : ndarray of shape (n_samples, )
             Responses of input data to fit to
-
         Notes
         -----
-
         """
-        pass
+        self.rf = RandomForestClassifier(n_estimators=4, random_state=0)
+        self.rf.fit(X, y)
+        #self.logisticRegr = LogisticRegression()
+        #self.logisticRegr.fit(X, y)
+
 
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """
         Predict responses for given samples using fitted estimator
-
         Parameters
         ----------
         X : ndarray of shape (n_samples, n_features)
             Input data to predict responses for
-
         Returns
         -------
         responses : ndarray of shape (n_samples, )
             Predicted responses of given samples
         """
-        return np.zeros(X.shape[0])
+        return self.rf.predict(X)
 
     def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
         """
         Evaluate performance under loss function
-
         Parameters
         ----------
         X : ndarray of shape (n_samples, n_features)
             Test samples
-
         y : ndarray of shape (n_samples, )
             True labels of test samples
-
         Returns
         -------
         loss : float
             Performance under loss function
         """
-        pass
+        return np.mean(abs(self.predict(X) - y))
